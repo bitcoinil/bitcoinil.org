81f2b159e3876fbeb529fbf709056a1f25b2b56c
Rick <rickbgove@gmail.com>
2022-07-07 10:05:42 +0200

    remove app-layout package

--- packages/app-layout/src/index.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react"
-import TestButton from "./TestButton"
-
-export { NewTabLink } from "./NewTabLink"
-// export { CounterButton } from "./CounterButton"
-export type { CounterButtonProps } from "./CounterButton"
-export { TestButton }
