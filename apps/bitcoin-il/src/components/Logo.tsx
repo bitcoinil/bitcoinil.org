476dc0cdd005432aee685d246612f38f72c63126
Rick <rickbgove@gmail.com>
2022-07-07 16:34:11 +0200

    Add dark mode theme option

--- apps/bitcoin-il/src/components/Logo.tsx
+++ apps/bitcoin-il/src/components/Logo.tsx
@@ -2,9 +2,15 @@ import * as React from 'react'
 import styled from 'styled-components'
 
 import LogoSVG from '../img/logo.svg'
+import darkLogoSVG from '../img/logo_dark_mode.svg'
 
-const Logo = (props: any) => {
-  return <StyledLogo src={LogoSVG} {...props} />
+export interface LogoProps {
+  isDark: boolean
+  props?: any
+}
+
+const Logo: React.FC<LogoProps> = ({ props, isDark }) => {
+  return <StyledLogo src={isDark ? darkLogoSVG : LogoSVG} {...props} />
 }
 
 const StyledLogo = styled.img`
