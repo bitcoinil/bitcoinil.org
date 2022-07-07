1b4f89301ce01337f4d6506f7adcfeebe83c05ea
Rick <rickbgove@gmail.com>
2022-07-07 10:07:46 +0200

    remove ui-next package

--- packages/ui-next/src/index.tsx
+++
@@ -1,4 +0,0 @@
-import * as React from "react"
-export { NewTabLink } from "./NewTabLink"
-// export { CounterButton } from "./CounterButton";
-// export type { CounterButtonProps } from "./CounterButton";
