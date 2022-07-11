729f557963660361eeb0c9ebfee441383462df55
Rick <rickbgove@gmail.com>
2022-07-11 17:22:44 +0200

    Scroll to top on new location; tsify index

--- apps/bitcoin-il/src/layout/index.tsx
+++ apps/bitcoin-il/src/layout/index.tsx
@@ -1,14 +1,23 @@
 import * as React from 'react'
+import { useLocation } from 'react-router-dom'
 import { useRecoilState } from 'recoil'
 import styled from 'styled-components'
+
 import Support from '../components/Support'
 import { currentlySelectedLanguage } from '../state/state'
 import { phoneDevices } from '../utils/breakpoints'
+import { AppLayoutProps } from '../utils/interfaces'
 import Footer from './Footer'
 import Header from './Header'
 
-const AppLayout = ({ children }) => {
+const AppLayout: React.FC<AppLayoutProps> = ({ children }) => {
   const [ln] = useRecoilState(currentlySelectedLanguage)
+
+  const location = useLocation()
+
+  React.useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+  }, [location])
 
   return (
     <AppStyleWrap id="app" className={ln.language === 'he' ? 'dir-rtl' : ''}>
