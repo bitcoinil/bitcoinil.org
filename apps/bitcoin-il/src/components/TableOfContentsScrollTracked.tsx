f1708f876e7f1589a6af70a422e8705636d58080
Rick <rickbgove@gmail.com>
2022-07-14 11:56:35 +0200

    Add border right to currently viewed element

--- apps/bitcoin-il/src/components/TableOfContentsScrollTracked.tsx
+++ apps/bitcoin-il/src/components/TableOfContentsScrollTracked.tsx
@@ -380,17 +380,6 @@ const StyledTableOfContentsScrollTracked = styled.div`
   }
 
   .blink {
-    animation: blink-animation 1s steps(5, start) infinite;
-    -webkit-animation: blink-animation 1s steps(5, start) infinite;
-  }
-  @keyframes blink-animation {
-    to {
-      visibility: hidden;
-    }
-  }
-  @-webkit-keyframes blink-animation {
-    to {
-      visibility: hidden;
-    }
+    border-right: 10px solid red;
   }
 `
