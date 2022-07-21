c6e7342170927609fb9034c9b89b0c91d8b3d048
Rick <rickbgove@gmail.com>
2022-07-21 12:44:16 +0200

    Revert "add colors for burger menu menu"
    This reverts commit 3c684e55a11f1ebaec44d23d8f8368872cd3672e.

--- apps/bitcoin-il/src/theme/colors.ts
+++ apps/bitcoin-il/src/theme/colors.ts
@@ -2,7 +2,5 @@ export const colors = {
   accent: '#00b3f0',
   dullAccent: '#FEF4E8',
   whiteText: '#FFFFFF',
-  borderColor: '#DADADA',
-  burgerMenuBg: '#13161f',
-  burgerMenuSubBg: '#090c14'
+  borderColor: '#DADADA'
 }
