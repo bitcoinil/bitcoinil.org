3c684e55a11f1ebaec44d23d8f8368872cd3672e
Rick <rickbgove@gmail.com>
2022-07-21 12:34:06 +0200

    add colors for burger menu menu

--- apps/bitcoin-il/src/theme/colors.ts
+++ apps/bitcoin-il/src/theme/colors.ts
@@ -2,5 +2,7 @@ export const colors = {
   accent: '#00b3f0',
   dullAccent: '#FEF4E8',
   whiteText: '#FFFFFF',
-  borderColor: '#DADADA'
+  borderColor: '#DADADA',
+  burgerMenuBg: '#13161f',
+  burgerMenuSubBg: '#090c14'
 }
