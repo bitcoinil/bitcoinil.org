3db5ef2ce672782c8b75478834e65245c62a37ee
Rick <rickbgove@gmail.com>
2022-08-04 06:38:52 +0200

    Footer logo size is better

--- apps/bitcoin-il/src/components/Logo.tsx
+++ apps/bitcoin-il/src/components/Logo.tsx
@@ -4,7 +4,19 @@ import { ReactComponent as LogoSVG } from '../img/logo.svg'
 import { LogoProps } from '../utils/interfaces'
 
 const Logo: React.FC<LogoProps> = ({ props, isDark }) => {
-  return <LogoSVG width={80} {...props} />
+  const [size, setSize] = React.useState(500)
+  console.log(window.innerWidth - 40)
+
+  React.useEffect(() => {
+    const resizeHandler = () => {
+      if (window.innerWidth < 500) setSize(window.innerWidth - 40)
+    }
+    window.addEventListener('resize', resizeHandler)
+
+    return () => window.removeEventListener('resize', resizeHandler)
+  }, [])
+
+  return <LogoSVG width={size} {...props} />
 }
 
 export default Logo
