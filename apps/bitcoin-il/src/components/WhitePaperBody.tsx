11cf14945148c943c39f103db182c0f5e98e105f
Eli Sklar <eli.sklar@gmail.com>
2022-08-11 14:24:26 +0300

    Added whitepaper n'stuff

--- apps/bitcoin-il/src/components/WhitePaperBody.tsx
+++ apps/bitcoin-il/src/components/WhitePaperBody.tsx
@@ -2,7 +2,7 @@ import { Card } from 'antd'
 import * as React from 'react'
 import styled from 'styled-components'
 
-import { whitePaperTranslations } from '../data/WhitePaperData'
+import { ilWhitePaperTranslations, whitePaperTranslations } from '../data/WhitePaperData'
 import ico_download from '../img/ico_download.svg'
 import { colors } from '../theme/colors'
 import { landscapeMobile, phoneDevices } from '../utils/breakpoints'
@@ -14,7 +14,57 @@ const WhitePaperBody: React.FC<WhitePaperBodyProps> = ({}) => {
     <StyledWhitePaperBody id="WhitePaperBody">
       <h1>
         <FormattedMessage
-          id={`white-paper.intro`}
+          id={`bitil-white-paper.intro`}
+          defaultMessage={`Bitcoin Israel's whitepaper is a low-technicalities document focusing on the usability and adoption of Bitcoin and described as a scaling attempt to the Bitcoin Core Consensus`}
+          description={`intro`}
+        />
+      </h1>
+      <div className="il-papers papers">
+        {ilWhitePaperTranslations.map((translation, i) => {
+          return (
+            <a key={i} href={translation.link}>
+              <Card>
+                <h1>
+                  <img src={ico_download} />
+                  {translation.language}
+                </h1>
+                {translation.translatedBy ? (
+                  <p>
+                    Translated: by{' '}
+                    {translation.translatedBy.map((translationSub, i) => {
+                      return (
+                        <span
+                          className="js-link"
+                          key={`tran-${i}`}
+                          onClick={() =>
+                            window.location.replace(translationSub.link)
+                          }
+                        >
+                          {translationSub.author}
+                        </span>
+                      )
+                    })}
+                  </p>
+                ) : (
+                  <p style={{ visibility: 'hidden' }}>spacer</p>
+                )}
+              </Card>
+            </a>
+          )
+        })}
+      </div>
+      <h2>
+        <FormattedMessage
+          id={`white-paper.do-you-want-translate`}
+          defaultMessage={`Do you want to translate the paper into your language? Visit the Bitcoin white paper repository on GitHub for instructions and open an issue if you have any questions.`}
+          description={`do-you-want-translate`}
+        />
+      </h2>
+
+      <hr />
+      <h1>
+        <FormattedMessage
+          id={`original-white-paper.intro`}
           defaultMessage={`Satoshi Nakamoto's original paper is still recommended reading for anyone studying how Bitcoin works. Choose which translation of the paper you want to read:`}
           description={`intro`}
         />
@@ -53,13 +103,6 @@ const WhitePaperBody: React.FC<WhitePaperBodyProps> = ({}) => {
           )
         })}
       </div>
-      <h1>
-        <FormattedMessage
-          id={`white-paper.do-you-want-translate`}
-          defaultMessage={`Do you want to translate the paper into your language? Visit the Bitcoin white paper repository on GitHub for instructions and open an issue if you have any questions.`}
-          description={`do-you-want-translate`}
-        />
-      </h1>
     </StyledWhitePaperBody>
   )
 }
