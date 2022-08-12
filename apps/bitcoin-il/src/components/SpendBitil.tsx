6bdbbad8f3d12acc9599c8860a8f413e046d6267
Rick <rickbgove@gmail.com>
2022-08-12 16:30:02 +0200

    init spend route

---
+++ apps/bitcoin-il/src/components/SpendBitil.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import styled from 'styled-components'
+
+export interface SpendBitilProps {}
+
+const SpendBitil: React.FC<SpendBitilProps> = ({}) => {
+  return (
+    <StyledSpendBitil id="SpendBitil">
+      <h1>SpendBitil</h1>
+    </StyledSpendBitil>
+  )
+}
+
+export default SpendBitil
+
+const StyledSpendBitil = styled.div``
