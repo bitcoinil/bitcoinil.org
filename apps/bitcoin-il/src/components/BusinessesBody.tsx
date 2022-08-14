9b3703cec1e91db8d371110d546e8286aadb83f0
Rick <rickbgove@gmail.com>
2022-08-14 08:04:50 +0200

    test site button link as prop

--- apps/bitcoin-il/src/components/BusinessesBody.tsx
+++ apps/bitcoin-il/src/components/BusinessesBody.tsx
@@ -14,7 +14,11 @@ const BusinessBody: React.FC<BusinessBodyProps> = ({}) => {
       <CardsDisplay cards={businessCards} />
       <div className="individuals-button">
         <CustomNavLink to="/getting-started">
-          <SiteButton type="primary">
+          <SiteButton
+            buttonLinkWrapUrl={`https://www.FooFighters.com`}
+            buttonLinkId={`Foo Fighters`}
+            type="primary"
+          >
             <FormattedMessage
               id={`businesses.get-started-button`}
               defaultMessage={`Get Started With BitCoin Il`}
